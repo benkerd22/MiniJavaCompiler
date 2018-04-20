2deb43fbf00cf8b72f1c4d78691df30cb6f43274
benker <benkerd22@gmail.com>
2018-04-20 15:04:25 +0800

    增加对Kanga编译正确性检查

--- judge/judge.bat
+++ judge/judge.bat
@@ -2,13 +2,13 @@
 echo Start
 cd ..
 for %%G in (samples\*.java) do (
-  echo ---- %%~nG ----
+  echo ---- Run %%~nG ----
   java -cp bin Main %%~fG
   echo.
 )
 
 for %%G in (samples\*.spg) do (
-  echo ---- %%~nG ----
+  echo ---- sPiglet %%~nG ----
   java -jar judge\spp.jar < %%~fG
   java -jar judge\pgi.jar < %%~fG > judge\tmp.txt
   javac -d judge\tmp %%~pG%%~nG.java
@@ -17,9 +17,19 @@ for %%G in (samples\*.spg) do (
   echo.
 )
 
+for %%G in (samples\*.kg) do (
+  echo ---- Kanga %%~nG ----
+  java -jar judge\kgi.jar < %%~fG > judge\tmp.txt
+  javac -d judge\tmp %%~pG%%~nG.java
+  java -cp judge\tmp %%~nG > judge\goal.txt
+  fc judge\tmp.txt judge\goal.txt
+  echo.
+)
+
 pause
 
-del /Q judge\tmp.txt
-del /Q judge\goal.txt
-del /S /Q judge\tmp
-rmdir /Q judge\tmp
+cd judge
+del /Q tmp.txt
+del /Q goal.txt
+del /S /Q tmp
+rmdir /Q tmp
