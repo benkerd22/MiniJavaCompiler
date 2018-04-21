a935ac2f8bf57520b47ffdde8742a16fc6c2afbe
benker <benkerd22@gmail.com>
2018-04-21 14:28:24 +0800

    优化Main入口流程

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,45 @@
 import minijava.typecheck.*;
 import minijava.minijava2piglet.*;
 import spiglet.spiglet2kanga.*;
+import kanga.kanga2MIPS.*;
 
 public class Main {
-	public static String ext(String filename) {
-		return filename.replace(".java", "");
+	private static int pos(String filename) {
+		int i = filename.lastIndexOf(".");
+		int p = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\'));
+
+		if (i > p)
+			return i;
+		return filename.length();
+	}
+
+	private static String name(String filename) {
+		return filename.substring(0, pos(filename));
+	}
+
+	private static String ext(String filename) {
+		return filename.substring(pos(filename));
 	}
 
 	public static void main(String[] args) {
 		try {
-			String def = "samples\\LinkedList.java"; // default source file
+			String f = "samples\\ccc.java"; // default source file
 			if (args.length > 0)
-				def = args[0];
+				f = args[0];
 
-			def = ext(def);
+			String n = name(f), e = ext(f);
+			switch (e) {
+			case ".java":
+				boolean succ = TypeCheck.check(f);
+				if (!succ)
+					return;
 
-			boolean succ = TypeCheck.check(def + ".java");
-			if (!succ)
-				return;
-
-			ToPiglet.compile(def + ".java", def + ".spg");
-
-			ToKanga.compile(def + ".spg", def + ".kg");
+				ToPiglet.compile(f, n + ".spg");
+			case ".spg":
+				ToKanga.compile(n + ".spg", n + ".kg");
+			case ".kg":
+				ToMIPS.compile(n + ".kg", n + ".s");
+			}
 		} catch (Exception e) {
 			System.out.println("Oops");
 			e.printStackTrace();
